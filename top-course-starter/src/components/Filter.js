f2c1aed8f09ae726616975499e5222d5b38cac6a
Snigdho Roy <Snighdho.Roy@lenze.com>
2025-07-22 16:16:53 +0530

    Merge pull request #1 from roysni/main
    Fixed issues

--- top-course-starter/src/components/Filter.js
+++ top-course-starter/src/components/Filter.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
-const Filter = ({filterData}) => {
+const Filter = ({ filterData }) => {
+
   return (
     <div>
-    
-    {filterData.map((data) => {
-           return( <button>
-                {data.title}
-            </button>)
-    })}
+
+      {filterData && Object.entries(filterData).map(([key, value]) => {
+        return (
+          <div key={key}>
+
+            <div className="btn">
+              {key}
+            </div>
+
+            <div className="menu-list">
+              {value.map((item) => {
+                return (
+                  <div>{item.title}</div>
+                )
+              })}
+            </div>
+
+          </div>
+        )
+      })
+
+      }
 
     </div>
   )
